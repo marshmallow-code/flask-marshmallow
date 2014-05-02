22ca3d24dbf03a6e434260ab096a4459a65e4aeb
Steven Loria <sloria1@gmail.com>
2014-05-01 23:28:51 -0400

    Tests next

--- test_flask_marshmallow.py
+++ test_flask_marshmallow.py
@@ -6,28 +6,32 @@ from flask import Flask, url_for
 from werkzeug.routing import BuildError
 from werkzeug.wrappers import BaseResponse
 
-from flask_marshmallow import Serializer, fields, pprint
-from flask_marshmallow.fields import Hyperlinks, URL, _tpl, AbsoluteURL
+from flask.ext.marshmallow import Marshmallow
+from flask.ext.marshmallow.fields import _tpl
+from flask.ext.marshmallow import exceptions
+
+_app = Flask('Testing app')
+
+@_app.route('/author/<int:id>')
+def author(id):
+    return 'Steven Pressfield'
+
+@_app.route('/authors/')
+def authors():
+    return 'Steven Pressfield, Chuck Paluhniuk'
+
+@_app.route('/books/')
+def books():
+    return 'Legend of Bagger Vance, Fight Club'
+
+@_app.route('/books/<id>')
+def book(id):
+    return 'Legend of Bagger Vance'
+
+mar = Marshmallow(_app)
 
 @pytest.yield_fixture(scope='function')
 def app():
-    _app = Flask('Testing app')
-
-    @_app.route('/author/<int:id>')
-    def author(id):
-        return 'Steven Pressfield'
-
-    @_app.route('/authors/')
-    def authors():
-        return 'Steven Pressfield, Chuck Paluhniuk'
-
-    @_app.route('/books/')
-    def books():
-        return 'Legend of Bagger Vance, Fight Club'
-
-    @_app.route('/books/<id>')
-    def book(id):
-        return 'Legend of Bagger Vance'
 
     ctx = _app.test_request_context()
     ctx.push()
@@ -35,6 +39,10 @@ def app():
     yield _app
 
     ctx.pop()
+
+@pytest.fixture(scope='function')
+def ma(app):
+    return Marshmallow(app)
 
 
 @pytest.fixture
@@ -66,28 +74,28 @@ def test_tpl(template):
     assert _tpl(template) == 'id'
     assert _tpl(template) == 'id'
 
-def test_url_field(app, mockauthor):
-    field = URL('author', id='<id>')
+def test_url_field(ma, mockauthor):
+    field = ma.URL('author', id='<id>')
     result = field.output('url', mockauthor)
     assert result == url_for('author', id=mockauthor.id)
 
-def test_url_field_with_invalid_attribute(app, mockauthor):
-    field = URL('author', id='<not-an-attr>')
+def test_url_field_with_invalid_attribute(ma, mockauthor):
+    field = ma.URL('author', id='<not-an-attr>')
     with pytest.raises(AttributeError) as excinfo:
         field.output('url', mockauthor)
     expected_msg = '{0!r} is not a valid attribute of {1!r}'.format(
         'not-an-attr', mockauthor)
     assert expected_msg in str(excinfo)
 
-def test_invalid_endpoint_raises_build_error(app, mockauthor):
-    field = URL('badendpoint')
+def test_invalid_endpoint_raises_build_error(ma, mockauthor):
+    field = ma.URL('badendpoint')
     with pytest.raises(BuildError):
         field.output('url', mockauthor)
 
-def test_hyperlinks_field(app, mockauthor):
-    field = Hyperlinks({
-        'self': URL('author', id='<id>'),
-        'collection': URL('authors')
+def test_hyperlinks_field(ma, mockauthor):
+    field = ma.Hyperlinks({
+        'self': ma.URL('author', id='<id>'),
+        'collection': ma.URL('authors')
     })
 
     result = field.output('_links', mockauthor)
@@ -96,14 +104,14 @@ def test_hyperlinks_field(app, mockauthor):
         'collection': url_for('authors')
     }
 
-def test_hyperlinks_field_recurses(app, mockauthor):
-    field = Hyperlinks({
+def test_hyperlinks_field_recurses(ma, mockauthor):
+    field = ma.Hyperlinks({
         'self': {
-            'href': URL('author', id='<id>'),
+            'href': ma.URL('author', id='<id>'),
             'title': 'The author'
         },
         'collection': {
-            'href': URL('authors'),
+            'href': ma.URL('authors'),
             'title': 'Authors list'
         }
     })
@@ -116,36 +124,43 @@ def test_hyperlinks_field_recurses(app, mockauthor):
                         'title': 'Authors list'}
     }
 
-def test_absolute_url(app, mockauthor):
-    field = AbsoluteURL('authors')
+def test_absolute_url(ma, mockauthor):
+    field = ma.AbsoluteURL('authors')
     result = field.output('abs_url', mockauthor)
     assert result == url_for('authors', _external=True)
 
-def test_aliases():
-    from flask_marshmallow.fields import Url, AbsoluteUrl
-    assert Url == URL
-    assert AbsoluteUrl == AbsoluteURL
+def test_deferred_initialization():
+    app = Flask(__name__)
+    m = Marshmallow()
+    m.init_app(app)
 
-class AuthorMarshal(Serializer):
+    assert 'flask-marshmallow' in app.extensions
+
+def test_aliases(ma):
+    from flask_marshmallow.fields import Url, AbsoluteUrl
+    assert Url == ma.URL
+    assert AbsoluteUrl == ma.AbsoluteURL
+
+class AuthorMarshal(mar.Serializer):
     class Meta:
         fields = ('id', 'name', 'absolute_url', 'links')
 
-    absolute_url = AbsoluteURL('author', id='<id>')
+    absolute_url = mar.AbsoluteURL('author', id='<id>')
 
-    links = fields.Hyperlinks({
-        'self': fields.URL('author', id='<id>'),
-        'collection': fields.URL('authors')
+    links = mar.Hyperlinks({
+        'self': mar.URL('author', id='<id>'),
+        'collection': mar.URL('authors')
     })
 
-class BookMarshal(Serializer):
+class BookMarshal(mar.Serializer):
     class Meta:
         fields = ('id', 'title', 'author', 'links')
 
-    author = fields.Nested(AuthorMarshal)
+    author = mar.Nested(AuthorMarshal)
 
-    links = fields.Hyperlinks({
-        'self': fields.URL('book', id='<id>'),
-        'collection': fields.URL('books'),
+    links = mar.Hyperlinks({
+        'self': mar.URL('book', id='<id>'),
+        'collection': mar.URL('books'),
     })
 
 def test_serializer(app, mockauthor):
@@ -170,3 +185,70 @@ def test_links_within_nested_object(app, mockbook):
     author = s.data['author']
     assert author['links']['self'] == url_for('author', id=mockbook.author.id)
     assert author['links']['collection'] == url_for('authors')
+
+class ConfigTestCase:
+
+    def set_config(self, app):
+        pass
+
+    def setup_method(self, method):
+        self.app = Flask(__name__)
+        self.set_config(self.app)
+
+        ma = Marshmallow()
+        ma.init_app(self.app)
+
+        class Marshal(ma.Serializer):
+            meaning = ma.Integer()
+
+        self.Marshal = Marshal
+
+        self.obj = mock.Mock()
+        self.obj.meaning = 42
+
+
+class TestStrictConfig(ConfigTestCase):
+
+    def set_config(self, app):
+        app.config['MARSHMALLOW_STRICT'] = True
+
+    def test_strict_mode_default(self):
+        with self.app.test_request_context():
+            serialized = self.Marshal(self.obj)
+            assert serialized.strict is True
+            with pytest.raises(exceptions.MarshallingError):
+                badobj = mock.Mock()
+                badobj.meaning = 'bad'
+                self.Marshal(badobj)
+
+    def test_strict_mode_override(self):
+        ma = Marshmallow(self.app)
+
+        class MyMarshal(ma.Serializer):
+            meaning = ma.Integer()
+            # override strict mode
+            class Meta:
+                strict = False
+
+        with self.app.test_request_context():
+            serialized = MyMarshal(self.obj)
+            assert serialized.strict is False
+
+class TestDateformatConfig(ConfigTestCase):
+
+    def set_config(self, app):
+        self.app.config['MARSHMALLOW_DATEFORMAT'] = 'iso'
+
+    def test_dateformat_default(self):
+        with self.app.test_request_context():
+            serialized = self.Marshal(self.obj)
+            assert serialized.opts.dateformat == 'iso'
+
+    def test_dateformat_override(self):
+        ma = Marshmallow(self.app)
+        class MyMarshal(self.Marshal):
+            class Meta:
+                dateformat = '%d'
+        with self.app.test_request_context():
+            s = MyMarshal(self.obj)
+            assert s.opts.dateformat == '%d'
