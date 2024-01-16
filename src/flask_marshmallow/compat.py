5f3fdfe23d5ae47de8afa9556bcaa6da04a8d18b
Steven Loria <sloria1@gmail.com>
2024-01-16 16:15:25 -0500

    Remove compat.py and unnecessary config (#286)

--- src/flask_marshmallow/compat.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility module.
-
-This module should be considered private API.
-"""
-import marshmallow
-
-_MARSHMALLOW_VERSION_INFO = tuple(
-    int(part) for part in marshmallow.__version__.split(".") if part.isdigit()
-)
